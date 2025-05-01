eec14ad21d6fdfd4367da6d5d849ea85a736be9c
RVMiranda <rafmicr884@gmail.com>
2025-05-01 16:51:28 -0600

    actualizar menú principal y estilos de navegación

--- src/pages/base/Menu.jsx
+++ src/pages/base/Menu.jsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom";
+import '../../styles/Menu.css';
 
 export default function Menu() {
     return (
-        <header>
-            <nav>
-                <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none' }}>
-                    <li><Link to="/">Inicio</Link></li>
-                </ul>
-                <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none' }}>
-                    <li><Link to="/characters">Personajes</Link></li>
-                </ul>
-                <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none' }}>
-                    <li><Link to="/busqueda">Busqueda de Personajes</Link></li>
-                </ul>
-            </nav>
+        <header className="menu-container">
+            <div className="menu-left">
+                <div className="hamburger">☰</div>
+            </div>
+            <div className="menu-logo">
+                <img src="/images/Logo.webp" alt="Rick and Morty"/>
+            </div>
+            <div className="menu-right">
+                <li><Link to="/">Inicio</Link></li>
+                <li><Link to="/characters">Personajes</Link></li>
+                <li><Link to="/busqueda">Busqueda de Personajes</Link></li>
+                <li><Link to="/favoritos">Personajes favoritos</Link></li>
+                <li><Link to="/extra">Pagina extra</Link></li>
+            </div>
+            
         </header>
     );
 }
