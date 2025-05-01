d184fdc54bc171b59951f12a36652f469bc325d2
RVMiranda <rafmicr884@gmail.com>
2025-05-01 13:44:21 -0600

    pequeños ajustes generales de estilos y configuraciones de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import EpisodeCard from './components/EpisodeCard'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [episodios, setEpisodios] = useState([])
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/episode')
+      .then((response) => response.json())
+      .then((data) => setEpisodios(data.results))
+      .catch((error) => console.error('Error fetching episodes:', error));
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app-container">
+        <h1>Episodios de Rick & Morty</h1>
+        <div className="grid-container">
+          {episodios.map(episodios => (
+            <EpisodeCard key={episodios.id} episode={episodios} />
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
